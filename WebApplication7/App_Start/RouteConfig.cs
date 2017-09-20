b303e142ffcc88ee8a371b5ecd5a394f70f01b6f
Alice McDowell <alice.mcdowell@icstars-columbus.org>
2017-09-20 09:32:47 -0400

    Added index files as well as a new controller

--- WebApplication7/App_Start/RouteConfig.cs
+++ WebApplication7/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace WebApplication7
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+      name: "Hello",
+      url: "{controller}/{action}/{name}/{id}"
+  );
         }
     }
 }
